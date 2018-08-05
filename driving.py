4810e92679dcf10adecc2b2905f5f72190d4a0db
Bei Song <vincent17.lz12@gmail.com>
2018-08-05 23:31:10 +0800

    add Taiwan &USA

---
+++ driving.py
@@ -0,0 +1,14 @@
+country = input ('請問您哪國人：')
+age = input ('請問您幾歲：')
+age = int(age)
+if country == '台灣':
+	if age >= 18 :
+		print ('那你可以考駕照嚕')
+	else:
+		print ('你還不考駕照喔')
+elif country == '美國':
+	if age >= 16:
+		print ('你可以考駕照喔')
+	else:
+		print ('你還不能考駕照喔')
+
